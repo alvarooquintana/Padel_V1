d571099e428c848219fa5d28acdd208dc313289a
Alvaro <42539212+alvarooquintana@users.noreply.github.com>
2023-02-10 14:39:30 +0100

    .

--- app/main.py
+++ app/main.py
@@ -1,54 +1,9 @@
-from fastapi import FastAPI, Request, status
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.templating import Jinja2Templates
-from fastapi.security import HTTPBasic
-from fastapi.staticfiles import StaticFiles
-
-from databases.database import engine
-from models.model import Match
 from routers import matches
 
 
 app = FastAPI(docs_url=None, redoc_url=None)
-security = HTTPBasic()
-
-templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-
-
-
-@app.get("/")
-async def index():
-    return RedirectResponse(url="login", status_code=status.HTTP_302_FOUND)
-
-@app.get("/registro")
-async def index(request: Request):
-    matches = await engine.find(Match)
-    return templates.TemplateResponse(
-        "registro.html", {"request": request, "matches": matches}
-    )
-
-
-@app.get("/login")
-async def login(request: Request):
-
-    return templates.TemplateResponse("login.html", {"request": request})
-
-
-@app.get("/gastos")
-async def index(request: Request):
-    matches = await engine.find(Match)
-    table_data = []
-    for match in matches:
-        table_data.append(match)
-    return templates.TemplateResponse(
-        "gastos.html",
-        {"request": request, "matches": matches, "table_data": table_data},
-    )
-
 
 origins = ["http://localhost:8000", "http://127.0.0.1:8000"]
 
