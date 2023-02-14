1f992cfb0308f24229a7d0ac313964a85b377a9b
Alvaro <42539212+alvarooquintana@users.noreply.github.com>
2023-02-14 14:38:30 +0100

    .

--- app/databases/database.py
+++ app/databases/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 password = os.getenv("MONGO_PASSWORD")
 
-#client = AsyncIOMotorClient(f"mongodb+srv://alvaro:{password}@data-base-padel.k5g0psy.mongodb.net/registro-padel")
+client = AsyncIOMotorClient(f"mongodb+srv://alvaro:{password}@data-base-padel.k5g0psy.mongodb.net/registro-padel")
 client = AsyncIOMotorClient("mongodb://localhost:27017")
 
 engine = AIOEngine(client=client)
