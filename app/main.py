9b349b66f9802812b3e1b4c5a78d2db080d684d4
Alvaro <42539212+alvarooquintana@users.noreply.github.com>
2023-02-16 18:53:22 +0100

    .

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from routers import matches,views
 
-
-app = FastAPI(docs_url=None, redoc_url=None)
+#docs_url=None, redoc_url=None
+app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 origins = ["http://localhost:8000", "http://127.0.0.1:8000"]
