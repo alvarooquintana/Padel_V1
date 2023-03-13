b538fe9ace27ccef02b857642fc51f8f2419c20c
Alvaro <42539212+alvarooquintana@users.noreply.github.com>
2023-03-13 22:27:20 +0100

    add elif in html gastos

--- app/databases/database.py
+++ app/databases/database.py
@@ -9,10 +9,11 @@ load_dotenv()
 user_db = os.getenv("USER_DB")
 password_db = os.getenv("MONGO_PASSWORD")
 
+"""
 client = AsyncIOMotorClient(
     f"mongodb+srv://{user_db}:{password_db}@padelapp.ikvhpet.mongodb.net/test"
 )
-
-#client = AsyncIOMotorClient("mongodb://localhost:27017")
+"""
+client = AsyncIOMotorClient("mongodb://localhost:27017")
 
 engine = AIOEngine(client=client)
